3c4c4553202b50c4e89e1cb58175810ffa23d946
“Erick” <144264479+erick718@users.noreply.github.com>
2025-09-24 20:15:19 -0400

    commit

--- src/main/java/com/fitness/dao/UserDAO.java
+++ src/main/java/com/fitness/dao/UserDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.fitness.model.User;
+import com.fitness.Model.User;
 
 public class UserDAO {
     private Connection getConnection() throws SQLException {
